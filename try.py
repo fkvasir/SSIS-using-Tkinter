df576a68e395c551b4807fb77fdc60b2eef25667
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2023-06-26 13:22:05 +0800

    ...

--- try.py
+++ try.py
@@ -1,213 +1,233 @@
 import csv
 import tkinter as tk
 from tkinter import messagebox
-import customtkinter
+from tkinter import ttk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
-app = customtkinter.CTk()
-app.geometry("1080x720")
-app.title("SSISv1")
 
-title = customtkinter.CTkLabel(app, text = "Simple Student Information System")
-title.pack(padx=10, pady= 10)
 
-# def add_student():
-#     name = entry_name.get()
-#     id_number = entry_id.get()
-#     year_level = entry_year.get()
-#     sex = entry_sex.get()
-#     courses = listbox_courses.get_selected_items()
+root = tk.Tk()
+root.title("SSIS v1.0")
+root.geometry("1360x680")
 
-#     if name and id_number and year_level and courses:
-#         with open('student_info.csv', 'a', newline='') as file:
-#             writer = csv.writer(file)
-#             writer.writerow([name, id_number, year_level,sex] + courses)
+title_label = tk.Label(root, text="Simple Student Information System v1.0", font=("Arial", 30,"bold"),border=12,relief=tk.GROOVE,bg="lightgrey")
+title_label.pack(side=tk.TOP,fill=tk.X)
+
+detail_frame = tk.LabelFrame(root, text="Information",font=("Arial",13),bd=12,relief=tk.GROOVE,bg="lightgrey")
+detail_frame.place(x=20,y=90,width=450,height=575)
+
+data_frame = tk.Frame(root, bg="lightgrey",relief=tk.GROOVE)
+data_frame.place(x=475,y=90,width=810,height=575)
+
+id= tk.StringVar()
+name=tk.StringVar()
+sex=tk.StringVar()
+year=tk.StringVar()
+search=tk.StringVar()
+course =tk.StringVar()
+
+
+def add_course():
+    course = entry_course.get()
+
+    if course:
+        listbox_courses.insert(tk.END, course)
+        entry_course.delete(0, tk.END)
+    else:
+        messagebox.showerror("Error", "Please enter a course.")
+
+def save_selection():
+    student_name = entry_name.get()
+    student_id = entry_id.get()
+    student_sex = entry_sex.get()
+    student_year = entry_year.get()
+
+    selected_courses = listbox_courses.curselection()
+
+    if student_name and student_id and student_sex and selected_courses and student_year:
+        courses = [listbox_courses.get(index) for index in selected_courses]
+        with open('student_courses.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([student_name] + courses + [student_sex] + [student_id]+ [student_year])
         
-#         messagebox.showinfo("Student Added", "Student has been added successfully.")
-#         clear_fields()
-#     else:
-#         messagebox.showerror("Error", "Please fill in all fields and select at least one course.")
+        messagebox.showinfo("Selection Saved", "Selection has been saved successfully.")
+        entry_name.delete(0, tk.END)
+        entry_id.delete(0,tk.END)
+        entry_sex.delete(0,tk.END)
+        entry_year.delete(0,tk.END)
+        listbox_courses.selection_clear(0, tk.END)
+    else:
+        messagebox.showerror("Error", "Please enter a student name, id and age and select at most one course.")
 
-# def search_students():
-#     search_query = entry_search.get()
+def remove_data():
+    selected_index = listbox_data.curselection()
 
-#     if search_query:
-#         listbox_students.clear()
-#         with open('student_info.csv', 'r') as file:
-#             reader = csv.reader(file)
-#             for row in reader:
-#                 if search_query in row:
-#                     listbox_students.insert(row)
-#     else:
-#         messagebox.showerror("Error", "Please enter a search query.")
-
-# def update_student():
-#     selected_data = listbox_students.get_selected_items()
-
-#     if selected_data:
-#         selected_name = selected_data[0]
-#         selected_id = selected_data[1]
-#         selected_year = selected_data[2]
-#         selected_courses = selected_data[3:]
-
-#         window_update = customtkinter.CTk()
-#         window_update.title("Update Student")
-
-#         frame_update = customtkinter.CTkFrame(window_update)
-#         frame_update.pack(pady=20)
-
-#         label_name = customtkinter.CTkLabel(frame_update, text="Name:")
-#         label_name.grid(row=0, column=0)
-#         entry_name = customtkinter.CTkEntry(frame_update)
-#         entry_name.grid(row=0, column=1)
-#         entry_name.set_text(selected_name)
-
-#         label_id = customtkinter.CTkLabel(frame_update, text="ID Number:")
-#         label_id.grid(row=1, column=0)
-#         entry_id = customtkinter.CTkEntry(frame_update)
-#         entry_id.grid(row=1, column=1)
-#         entry_id.set_text(selected_id)
-
-#         label_year = customtkinter.CTkLabel(frame_update, text="Year Level:")
-#         label_year.grid(row=2, column=0)
-#         entry_year = customtkinter.CTkEntry(frame_update)
-#         entry_year.grid(row=2, column=1)
-#         entry_year.set_text(selected_year)
-
-#         label_courses = customtkinter.CTkLabel(frame_update, text="Courses:")
-#         label_courses.grid(row=3, column=0)
-#         entry_courses = customtkinter.CTkEntry(frame_update)
-#         entry_courses.grid(row=3, column=1)
-#         entry_courses.set_text(', '.join(selected_courses))
-
-#         def save_changes():
-#             new_name = entry_name.get_text()
-#             new_id = entry_id.get_text()
-#             new_year = entry_year.get_text()
-#             new_courses = entry_courses.get_text().split(', ')
-#             updated_data = [new_name, new_id, new_year] + new_courses
-
-#             with open('student_info.csv', 'r') as file:
-#                 records = list(csv.reader(file))
+    if selected_index:
+        confirmation = messagebox.askyesno("Confirm Deletion", "Are you sure you want to delete this record?")
+        
+        if confirmation:
+            with open('student_courses.csv', 'r') as file:
+                records = list(csv.reader(file))
             
-#             with open('student_info.csv', 'w', newline='') as file:
-#                 writer = csv.writer(file)
-#                 for row in records:
-#                     if row != selected_data:
-#                         writer.writerow(row)
-#                 writer.writerow(updated_data)
-
-#             messagebox.showinfo("Changes Saved", "Changes have been saved successfully.")
-#             window_update.destroy()
-#             load_students()
-
-#         button_save = customtkinter.CTkButton(window_update, text="Save Changes", command=save_changes)
-#         button_save.pack(pady=10)
-
-#     else:
-#         messagebox.showerror("Error", "Please select a student to update.")
-
-# def delete_student():
-#     selected_data = listbox_students.get_selected_items()
-
-#     if selected_data:
-#         confirmation = messagebox.askyesno("Confirm Deletion", "Are you sure you want to delete this student?")
-#         if confirmation:
-#             with open('student_info.csv', 'r') as file:
-#                 records = list(csv.reader(file))
+            del records[selected_index[0]]
             
-#             with open('student_info.csv', 'w', newline='') as file:
-#                 writer = csv.writer(file)
-#                 for row in records:
-#                     if row != selected_data:
-#                         writer.writerow(row)
+            with open('student_courses.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(records)
             
-#             messagebox.showinfo("Deletion Successful", "Student has been deleted.")
-#             load_students()
+            messagebox.showinfo("Data Removed", "Data has been removed successfully.")
+            load_data()
+    else:
+        messagebox.showerror("Error", "Please select a record to remove.")
 
-#     else:
-#         messagebox.showerror("Error", "Please select a student to delete.")
+def load_data():
+    listbox_data.delete(0, tk.END)
 
-# def clear_fields():
-#     entry_name.delete(0, tk.END)
-#     entry_id.delete(0, tk.END)
-#     entry_year.delete(0, tk.END)
-#     listbox_courses.clear_selection()
+    with open('student_courses.csv', 'r') as file:
+        records = csv.reader(file)
+        for row in records:
+            listbox_data.insert(tk.END, ', '.join(row))
 
-# def load_students():
-#     listbox_students.clear()
-#     with open('student_info.csv', 'r') as file:
-#         reader = csv.reader(file)
-#         for row in reader:
-#             listbox_students.insert(row)
+def edit_data():
+    selected_index = listbox_data.curselection()
 
-frame_courses = customtkinter.CTkFrame(app)
-frame_courses.pack(pady=10)
+    if selected_index:
+        selected_data = listbox_data.get(selected_index)
+        selected_data = selected_data.split(', ')
 
-label_courses = customtkinter.CTkLabel(frame_courses, text="Courses:")
-label_courses.pack(pady=10)
+        window_edit = tk.Toplevel()
+        window_edit.title("Edit Data")
 
-courses = customtkinter.CTkFrame(frame_courses)
-courses.pack(pady=10)
+        frame_edit = tk.Frame(window_edit)
+        frame_edit.pack(pady=20)
 
-frame_student = customtkinter.CTkFrame(app)
-frame_student.pack(pady=20)
+        label_name = tk.Label(frame_edit, text="Student Name:")
+        label_name.grid(row=0, column=0)
+        entry_name = tk.Entry(frame_edit)
+        entry_name.grid(row=0, column=1)
+        entry_name.insert(tk.END, selected_data[0])
 
-label_name = customtkinter.CTkLabel(frame_student, text="Name:")
-label_name.grid(row=0, column=0)
-entry_name = customtkinter.CTkEntry(frame_student)
-entry_name.grid(row=0, column=1)
+        label_courses = tk.Label(frame_edit, text="Courses:")
+        label_courses.grid(row=1, column=0)
+        entry_courses = tk.Entry(frame_edit)
+        entry_courses.grid(row=1, column=1)
+        entry_courses.insert(tk.END, ', '.join(selected_data[1:2]))
 
-label_id = customtkinter.CTkLabel(frame_student, text="ID Number:")
-label_id.grid(row=1, column=0)
-entry_id = customtkinter.CTkEntry(frame_student)
-entry_id.grid(row=1, column=1)
+        label_id = tk.Label(frame_edit,text="ID no.:")
+        label_id.grid(row = 2, column = 0)
+        entry_id = tk.Entry(frame_edit)
+        entry_id.grid(row = 2, column = 1)
+        entry_id.insert(tk.END, ', '.join(selected_data[2:3]))
 
-label_year = customtkinter.CTkLabel(frame_student, text="Year Level:")
-label_year.grid(row=2, column=0)
-entry_year = customtkinter.CTkEntry(frame_student)
-entry_year.grid(row=2, column=1)
+        label_age = tk.Label(frame_edit, text = "Age:" )
+        label_age.grid(row = 3, column = 0)
+        entry_age = tk.Entry(frame_edit)
+        entry_age.grid(row = 3, column = 1)
+        entry_age.insert(tk.END, ', '.join(selected_data[3:]))
+        
+        label_year = tk.Label(frame_edit, text = "Year:" )
+        label_year.grid(row = 3, column = 0)
+        entry_year = tk.Entry(frame_edit)
+        entry_year.grid(row = 3, column = 1)
+        entry_year.insert(tk.END, ', '.join(selected_data[3:]))
+
+        button_save = tk.Button(window_edit, text="Save Changes", command=lambda: save_changes(selected_index))
+        button_save.pack(pady=10)
+
+    else:
+        messagebox.showerror("Error", "Please select a record to edit.")
+
+def save_changes(selected_index):
+    new_name = entry_name.get()
+    new_courses = entry_course.get().split(', ')
+    new_id = entry_id.get().split(', ')
+    new_sex = entry_sex.get().split(', ')
+    new_year = entry_year.get().split(', ')
+
+    with open('student_courses.csv', 'r') as file:
+        records = list(csv.reader(file))
+
+    records[selected_index][1:1] = new_name
+    records[selected_index][1:2] = new_courses
+    records[selected_index][2:3] = new_sex
+    records[selected_index][3:4] = new_id
+    records[selected_index][4:4] = new_year
+
+    with open('student_courses.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(records)
+
+    messagebox.showinfo("Changes Saved", "Changes have been saved successfully.")
+    entry_name.delete(0, tk.END)
+    entry_courses.delete(0, tk.END)
+    load_data()
 
 
-# listbox_courses = customtkinter.CListBox(frame_courses, width=50)
-# listbox_courses.pack(pady=5)
 
-frame_buttons = customtkinter.CTkFrame(app)
+
+
+
+label_id = tk.Label(detail_frame, text="ID no.",font=("Arial", 13), bg= "lightgrey")
+label_id.grid(row=0,column=0,padx=2,pady=2)
+label_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=id)
+label_entry.grid(row=0,column=1,padx=2,pady=2)
+
+
+label_name = tk.Label(detail_frame, text="Name",font=("Arial", 13), bg= "lightgrey")
+label_name.grid(row=1,column=0,padx=2,pady=10)
+entry_name = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=name)
+entry_name.grid(row=1 ,column=1,padx=2,pady=10)
+
+
+listbox_data = tk.Listbox(root, width=50)
+listbox_data.pack(pady=10)
+
+sex_label = tk.Label(detail_frame, text="Sex",font=("Arial", 13), bg= "lightgrey")
+sex_label.grid(row=2,column=0,padx=2,pady=10)
+sex_entry = ttk.Combobox(detail_frame,font=("Arial",11),textvariable=sex)
+sex_entry['values']=("Male","Female")
+sex_entry.grid(row=2,column=1,padx=2,pady=10)
+
+listbox_courses = tk.Listbox(detail_frame, width=50)
+listbox_courses.grid(row=4,column=1,padx=2,pady=10)
+
+label_course = tk.Label(detail_frame, text="Course:", font=("Arial",13),bg="lightgrey")
+label_course.grid(row=3, column=0,padx=1,pady=10)
+entry_course = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=course)
+entry_course.grid(row=3, column=1, padx=1, pady=10)
+
+
+label_year = tk.Label(detail_frame, text="Year level:",font=("Arial",13),bg="lightgrey")
+label_year.grid(row=5, column=0)
+entry_year = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=course)
+entry_year.grid(row=5, column=1)
+
+frame_buttons = tk.Frame(root)
 frame_buttons.pack(pady=10)
 
-button_add_student = customtkinter.CTkButton(frame_buttons, text="Add Student")
-button_add_student.grid(row=0, column=0, padx=5)
+button_add_course = tk.Button(detail_frame, text="Add Course", command=add_course)
+button_add_course.place(x=345,y=155)
 
-button_search_students = customtkinter.CTkButton(frame_buttons, text="Search Students")
-button_search_students.grid(row=0, column=1, padx=5)
+btn_frame= tk.Frame(detail_frame, bg="lightgrey",bd=10,relief=tk.GROOVE)
+btn_frame.place(x=40,y=390,width=342,height=120)
 
-button_update_student = customtkinter.CTkButton(frame_buttons, text="Update Student")
-button_update_student.grid(row=0, column=2, padx=5)
+button_save_selection = tk.Button(btn_frame, text="Save",bg="lightgrey",bd=7,font=("Arial",13),width=15, command=save_selection)
+button_save_selection.grid(row=0,column=0,padx=2,pady=2)
 
-button_delete_student = customtkinter.CTkButton(frame_buttons, text="Delete Student")
-button_delete_student.grid(row=0, column=3, padx=5)
 
-frame_search = customtkinter.CTkFrame(app)
-frame_search.pack(pady=20)
+button_edit_data = tk.Button(btn_frame, bg="lightgrey", text="Edit",bd=7,font=("Arial",13),width=15,command=edit_data)
+button_edit_data.grid(row=0, column=1, padx=5)
 
-label_search = customtkinter.CTkLabel(frame_search, text="Search:")
-label_search.grid(row=0, column=0)
-entry_search = customtkinter.CTkEntry(frame_search)
-entry_search.grid(row=0, column=1)
+delete_btn = tk.Button(btn_frame,bg="lightgrey",text="DELETE",bd=7,font=("Arial",13),width=15)
+delete_btn.grid(row=1,column=0,padx=2,pady=2)
 
-button_clear_fields = customtkinter.CTkButton(app, text="Clear Fields")
-button_clear_fields.pack(pady=10)
+clear_btn = tk.Button(btn_frame, bg="lightgrey", text="CLEAR",bd=7,font=("Arial",13),width=15)
+clear_btn.grid(row=1,column=1,padx=3,pady=2)
 
-frame_students = customtkinter.CTkFrame(app)
-frame_students.pack(pady=20)
+button_remove_data = tk.Button(frame_buttons, text="Remove Data", command=remove_data)
+button_remove_data.grid(row=0, column=0, padx=5)
 
-label_students = customtkinter.CTkLabel(frame_students, text="Students:")
-label_students.pack()
 
-listbox_students = customtkinter.CTkFrame(frame_students)
-listbox_students.pack(pady=20)
 
-# load_students()
+load_data()
 
-app.mainloop()
+root.mainloop()
