46bb90429e459c38f9ab32aa6d28783f2b925c71
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2023-07-04 05:55:24 +0800

    ...

--- SSISv2.2.py
+++ SSISv2.2.py
@@ -8,8 +8,10 @@ from tkinter import messagebox
 
   
 app = tk.Tk()
-app.geometry("1300x680")
+app.geometry("1300x650")
 app.title("SSIS version 2.0")
+
+
 
 
 # placeholders for entries
@@ -26,17 +28,17 @@ coursecode=tk.StringVar()
 def setph(word,num):
     if num == 1:
         id.set(word)
-    if num == 1:
+    if num == 2:
         name.set(word)
-    if num == 1:
+    if num == 3:
         sex.set(word)
-    if num == 1:
+    if num == 4:
         year.set(word)
-    if num == 1:
+    if num == 5:
         course.set(word)
-    if num == 1:
+    if num == 6:
         searchin.set(word)
-    if num == 1:
+    if num == 7:
         coursecode.set(word)
         
 def read():
@@ -49,11 +51,11 @@ def read():
     return results
 
 def add(name, id_number, gender, year_level, course):
-    id = str(id_entry.get())
-    name = str(name_entry.get())
-    sex = str(sex_entry.get())
-    course = str(course_entry.get())
-    year = str(year_entry.get())
+    id_number = id.get()
+    name = name.get()
+    gender = sex.get()
+    year_level = year.get()
+    course = course.get()
 
     # Connect to database
     conn = sqlite3.connect('students.db')
@@ -157,8 +159,8 @@ def update():
 # Function to refresh the data in the tree view
 def refresh_data():
     # Clear the tree view
-    stud_table.delete(*stud_table.get_children())
-
+    for item in stud_table.get_children():
+        stud_table.delete(item)
     
     results = read()
 
@@ -191,6 +193,9 @@ def search():
     conn.close()
     
 def add_course(course_code, course_name):
+    course_code = coursecode_entry.get()
+    course_name = course_entry.get()
+    
     conn = sqlite3.connect('courses.db')
     cursor = conn.cursor()
     
@@ -257,91 +262,60 @@ def search_course(search_term):
     return results
 
 
-def open_courses_window():
-    # a new window
-    courses_window = tk.Toplevel()
-    courses_window.title("Courses")
-    
-    # Create a TreeView widget
-    courses_treeview = ttk.Treeview(courses_window)
-    courses_treeview["columns"] = ("course_code", "course_name")
-    
-    # Configure column names and properties
-    courses_treeview.column("#0", width=0, stretch=tk.NO)
-    courses_treeview.column("course_code", width=100, anchor=tk.CENTER)
-    courses_treeview.column("course_name", width=200, anchor=tk.W)
-    
-    courses_treeview.heading("#0", text="")
-    courses_treeview.heading("course_code", text="Course Code")
-    courses_treeview.heading("course_name", text="Course Name")
-    
-    # Populate the TreeView with course data
-    conn = sqlite3.connect('courses.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM courses")
-    courses = cursor.fetchall()
-    conn.close()
-    
-    for course in courses:
-        courses_treeview.insert("", tk.END, values=course)
-    
-    # Add the TreeView to the window
-    courses_treeview.pack(fill=tk.BOTH, expand=True)
-
 
 
 # Graphical User Interface
 
-title_label = tk.Label(app, text="Simple Student Information System v2.0", font=("Arial", 30, "bold"), border=12, relief= tk.GROOVE, bg = "lightgrey")
+title_label = tk.Label(app, border=5, height=20, relief= tk.GROOVE, bg = "lightblue")
 title_label.pack(side=tk.TOP,fill=tk.X)
 
 # frames
-detail_frame = tk.LabelFrame(app, text="Information",font=("Arial",20),bd=12,relief=tk.GROOVE,bg="lightgrey")
-detail_frame.place(x=20,y=90,width=450,height=575)
+detail_frame = tk.LabelFrame(app, bd=10,relief=tk.GROOVE,bg="lightblue")
+detail_frame.place(x=20,y=55,width=450,height=575)
 
-data_frame = tk.Frame(app, bg="lightgrey",relief=tk.GROOVE)
-data_frame.place(x=475,y=90,width=810,height=575)
+data_frame = tk.Frame(app, bg="lightblue",relief=tk.GROOVE)
+data_frame.place(x=475,y=55,width=810,height=575)
 
 
 
 
 # entries and labels >> detail frame
-id_label = tk.Label(detail_frame, text="ID",font=("Arial", 13), bg= "lightgrey")
-id_label.grid(row=0,column=0,padx=2,pady=10)
-id_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=id)
-id_entry.grid(row=0,column=1,padx=2,pady=10)
+id_label = tk.Label(detail_frame, text="ID",font=("Times", 10), bg= "lightblue")
+id_label.place(x=20,y=20)
+id_entry = tk.Entry(detail_frame,bd=5,font=("Times",10),textvariable=id)
+id_entry.place(x=110,y=20)
 
-name_label = tk.Label(detail_frame, text="Name",font=("Arial", 13), bg= "lightgrey")
-name_label.grid(row=1,column=0,padx=2,pady=10)
-name_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=name)
-name_entry.grid(row=1 ,column=1,padx=2,pady=10)
+name_label = tk.Label(detail_frame, text="Name",font=("Times", 10), bg= "lightblue")
+name_label.place(x=20,y=60)
+name_entry = tk.Entry(detail_frame,bd=5,font=("Times",10),textvariable=name)
+name_entry.place(x=110, y=60)
 
-sex_label = tk.Label(detail_frame, text="Sex",font=("Arial", 13), bg= "lightgrey")
-sex_label.grid(row=2,column=0,padx=2,pady=10)
-sex_entry = ttk.Combobox(detail_frame,font=("Arial",11),textvariable=sex,)
+sex_label = tk.Label(detail_frame, text="Sex",font=("Times", 10), bg= "lightblue")
+sex_label.place(x=20,y=110)
+sex_entry = ttk.Combobox(detail_frame,font=("Times",8),textvariable=sex,)
 sex_entry['values']=("Male","Female")
-sex_entry.grid(row=2,column=1,padx=2,pady=10)
+sex_entry.place(x=110,y=110)
 
 
-year_label = tk.Label(detail_frame, text="Year level",font=("Arial",13),bg="lightgrey")
-year_label.grid(row=5, column=0,padx=10,pady=10)
-year_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=year)
-year_entry.grid(row=5, column=1,padx=10,pady=10)
+year_label = tk.Label(detail_frame, text="Year level",font=("Times",10),bg="lightblue")
+year_label.place(x=20,y=400)
+year_entry = tk.Entry(detail_frame,bd=5,font=("Times",10),textvariable=year)
+year_entry.place(x=110,y=400)
 
 
 # Courses
-course_button = tk.Button(detail_frame, text="Courses",bg="lightgrey",bd=7,font=("Arial",7),width=10, command=open_courses_window)
-course_button.grid(row=3,column=0,padx=10,pady=10)
-coursecode_entry= tk.Label(detail_frame, text="Course Code",bg="lightgrey",bd=7,font=("Arial",13))
-coursecode_entry.grid(row=4,column=0,padx=10,pady=10)
+course_label = tk.Label(detail_frame, text="Course Name",bg="lightblue",bd=5,font=("Times",10),width=10)
+course_label.place(x=20,y=150)
+coursecode_entry= tk.Label(detail_frame, text="Course Code",bg="lightblue",bd=5,font=("Times",10))
+coursecode_entry.place(x=20,y=200)
 
-course_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=course)
-course_entry.grid(row=3, column=1, padx=10, pady=10)
-coursecode_entry = tk.Entry(detail_frame,bd=7,font=("Arial",13),textvariable=coursecode)
-coursecode_entry.grid(row=4, column=1, padx=10, pady=10)
+course_entry = tk.Entry(detail_frame,bd=5,font=("Times",10),textvariable=course)
+course_entry.place(x=110,y=150)
+coursecode_entry = tk.Entry(detail_frame,bd=5,font=("Times",10),textvariable=coursecode)
+coursecode_entry.place(x=110, y=200)
 
 
-button_add_course = tk.Button(detail_frame, text="Add Course",bg="lightgrey",bd=7,font=("Arial",7),width=10, command=add_course)
+button_add_course = tk.Button(detail_frame, text="Add Course",bg="lightgrey",bd=5,font=("Times",7),width=10, command=add_course)
 button_add_course.place(x=342,y=167)
 
 
@@ -350,14 +324,14 @@ button_add_course.place(x=342,y=167)
 
 # button frame
 btn_frame= tk.Frame(detail_frame, bg="lightgrey",bd=10,relief=tk.GROOVE)
-btn_frame.place(x=100,y=460,width=270,height=55)
+btn_frame.place(x=120,y=460,width=230,height=55)
 
 # buttons >> btn frame
-add_btn = tk.Button(btn_frame, text="Add",bg="lightgrey",bd=7,font=("Arial",7),width=15, command=add)
+add_btn = tk.Button(btn_frame, text="Add",bg="lightgrey",bd=5,font=("Times",7),width=15, command=add)
 add_btn.grid(row=0,column=0,padx=7,pady=2)
 
 
-delete_btn = tk.Button(btn_frame, bg="lightgrey", text="Delete",bd=7,font=("Arial",7),width=15,command=delete )
+delete_btn = tk.Button(btn_frame, bg="lightgrey", text="Delete",bd=5,font=("Times",7),width=15,command=delete )
 delete_btn.grid(row=0,column=1,padx=7,pady=2)
 
 
@@ -368,24 +342,24 @@ search_frame = tk.Frame(data_frame,bg="lightgrey",bd=10,relief=tk.GROOVE)
 search_frame.pack(side=tk.TOP, fill=tk.X)
 
 # search and show buttons
-search_label=tk.Label(search_frame,text="Search",bg="lightgrey",font=("Arial",14))
+search_label=tk.Label(title_label,text="Search",bg="lightblue",font=("Times",17))
 search_label.grid(row=0,column=0,padx=2,pady=2)
 
-search_entry =ttk.Entry(search_frame,font=("Arial", 14),textvariable=searchin)
+search_entry =ttk.Entry(title_label,font=("Times", 15),textvariable=searchin)
 search_entry.grid(row=0,column=1,padx=12,pady=2)
 
-search_btn = tk.Button(search_frame,bg="lightgrey",text="Search",bd=7,font=("Arial",7),width=15,command=search)
-search_btn.place(x=385,y=2)
+search_btn = tk.Button(title_label,bg="lightgrey",text="Search",bd=5,font=("Times",7),width=15,command=search)
+search_btn.place(x=880,y=5)
 
-update_button = tk.Button(search_frame, bg="lightgrey", text="Edit",bd=7,font=("Arial",7),width=15,command=update)
-update_button.place(x=510,y=2)
+update_button = tk.Button(title_label, bg="lightgrey", text="Edit",bd=5,font=("Times",7),width=15,command=update)
+update_button.place(x=980,y=5)
 
-reset_btn = tk.Button(search_frame, text="Reset",bg="lightgrey", bd=7,font=("Arial",7),width=15, command=reset_data)
-reset_btn.place(x=630,y=2)
+reset_btn = tk.Button(title_label, text="Reset",bg="lightgrey", bd=5,font=("Times",7),width=15, command=reset_data)
+reset_btn.place(x=1080,y=5)
 
 
 # frame for treeview
-main_frame=tk.Frame(data_frame,bg="lightgrey", bd=11,relief=tk.GROOVE)
+main_frame=tk.Frame(data_frame,bg="lightgrey", bd=7,relief=tk.GROOVE)
 main_frame.pack(fill=tk.BOTH,expand=True)
 
 y_scroll = tk.Scrollbar(main_frame, orient = tk.VERTICAL)
